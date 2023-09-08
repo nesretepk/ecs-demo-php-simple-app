313864a32f1d278b18fbaf3f35e2be35d348ac6a
lmegviar <lmegviar@gmail.com>
2023-09-08 19:25:08 -0400

    Try again with complete Dockerfile/setup.sh/boot to launch runit

--- setup.sh
+++ setup.sh
@@ -3,19 +3,19 @@
 apt-get update && apt-get -y upgrade
 # install required packages for LAMP and supporting packages
 apt-get install -y software-properties-common dirmngr vim net-tools sudo wget curl apt-utils awscli
-apt-get install -y apache2 mysql-server
+apt-get install -y mysql-server
 # install php and its extensions
-apt-get install -y php8.2 php8.2-mysql php8.2-curl php8.2-gd php8.2-mbstring php8.2-xml php8.2-xmlrpc php8.2-intl php8.2-zip libapache2-mod-php8.2
+# apt-get install -y php8.2 php8.2-mysql php8.2-curl php8.2-gd php8.2-mbstring php8.2-xml php8.2-xmlrpc php8.2-intl php8.2-zip libapache2-mod-php8.2
 # connect to docker repo
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
+# mkdir -p /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
 # start mysql and sleep for a while, so database can be created
 usermod -d -y /var/lib/mysql/ mysql
 echo "added?"
 service mysql start &
 sleep 5
-# creating database for wordpress
+# set root to blank pwd
 echo "UPDATE mysql.user SET authentication_string='' WHERE user='root'" | mysql
