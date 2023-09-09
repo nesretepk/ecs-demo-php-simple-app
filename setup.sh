0c049384e2fa5974349ebfb2abc70ba2a213aedc
lmegviar <lmegviar@gmail.com>
2023-09-08 20:57:06 -0400

    fix mysql install package name

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 # upgrade our system followed by update
 apt-get update && apt-get -y upgrade
 # install required packages for LAMP and supporting packages
-apt-get install -y software-properties-common dirmngr vim net-tools sudo wget curl apt-utils awscli
-apt-get install -y mysql-server
+apt-get update && install -y software-properties-common dirmngr vim net-tools sudo wget curl apt-utils awscli
+apt-get update && install -y default-mysql-server
 # install php and its extensions
 # apt-get install -y php8.2 php8.2-mysql php8.2-curl php8.2-gd php8.2-mbstring php8.2-xml php8.2-xmlrpc php8.2-intl php8.2-zip libapache2-mod-php8.2
 # connect to docker repo
