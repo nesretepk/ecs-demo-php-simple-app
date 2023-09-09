a078b394aa62381f1749e118dc0dfc5f40805183
lmegviar <lmegviar@gmail.com>
2023-09-09 18:04:15 -0400

    backto ubuntu?

--- setup.sh
+++ setup.sh
@@ -3,17 +3,17 @@
 apt-get update && apt-get -y upgrade
 # install required packages for LAMP and supporting packages
 apt-get update && install -y software-properties-common dirmngr vim net-tools sudo wget curl apt-utils awscli default-mysql-server
-# install php and its extensions
-# apt-get install -y php8.2 php8.2-mysql php8.2-curl php8.2-gd php8.2-mbstring php8.2-xml php8.2-xmlrpc php8.2-intl php8.2-zip libapache2-mod-php8.2
+install php and its extensions
+apt-get install -y php8.2 php8.2-mysql php8.2-curl php8.2-gd php8.2-mbstring php8.2-xml php8.2-xmlrpc php8.2-intl php8.2-zip libapache2-mod-php8.2
 # connect to docker repo
 # mkdir -p /etc/apt/keyrings
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 # echo \
 #   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 #   $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
-# start mysql and sleep for a while, so database can be created
+start mysql and sleep for a while, so database can be created
 usermod -d -y /var/lib/mysql/ mysql
-echo "added?"
+echo "addedy?"
 service mysql start &
 sleep 5
 # set root to blank pwd
